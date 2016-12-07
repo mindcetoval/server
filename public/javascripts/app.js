b535d810d09f0d4f85dfe45a69d99336dc9a538d
Adir Sation <adirush120@gmail.com>
2016-12-07 17:39:34 +0200

    adding files

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,11 @@
-var app = angular.module('corbisApp',[]);
+var app = angular.module('corbisApp',['ngRoute']);
 
 app.controller('mainCtrl',['$scope', function($scope) {
 }]);
 
-app.config
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/main/main.html',
+        controller: 'introCtrl'
+    })
+});
---
+++ public/views/main/main.js
@@ -0,0 +1,3 @@
+app.controller('introCtrl', ['$scope', function($scope) {
+    
+}]);
