379bc1612c69cdf36b0bcfc4eb668d2fa210d0cc
Ofer Rosenbaum <oferros1@gmail.com>
2016-12-08 01:48:03 +0200

    finalizing main animation

--- public/views/main/main.js
+++ public/views/main/main.js
@@ -34,7 +34,12 @@ app.controller('introCtrl', ['$scope', function ($scope) {
      //createjs.Ticker.addEventListener("tick", handleTick);
      stage.update();*/
     createjs.Ticker.addEventListener("tick", stage);
-    
+
+
+    function dropsShadow(event) {
+        myInterval = setInterval(zoomWorld, 100)
+            // event.currentTarget.shadow = new createjs.Shadow("#000000", 5, 5, 10);
+    }
 
     function dropsShadow(event) {
         myInterval = setInterval(zoomWorld, 100)
@@ -42,19 +47,24 @@ app.controller('introCtrl', ['$scope', function ($scope) {
     }
 
     function zoomWorld() {
-        if (counterForInterval == 4) //**when the animation is ending.**
+        if (counterForInterval == 3) //**when the animation is ending.**
         {
             clearInterval(myInterval);
 
         }
         if (counterForInterval < 3) {
-            bitmapZone.x -= 100;
-            bitmapZone.y -= 100;
+            bitmapZone.x -= 50;
+            bitmapZone.y -= 50;
+            //bitmapWorld.y-=100;
+            //bitmapWorld.x-=100;
         } else {
 
-            bitmapZone.x -= 640;
-            bitmapZone.y -= 640;
+            bitmapZone.x -= 100;
+            bitmapZone.y -= 100;
+            //bitmapWorld.x-=640;
+            //bitmapWorld.y-=640;
         }
+
         counterForInterval++;
         //bitmapWorld.x-=20;
 
@@ -62,14 +72,15 @@ app.controller('introCtrl', ['$scope', function ($scope) {
         console.log(document.getElementById("myCanvas").x);
         document.getElementById("myCanvas").style.marginLeft-=40;
         //document.getElementById("myCanvas").y-=40;*/
-
+        bitmapZone.scaleX *= 1.6;
+        bitmapZone.scaleY *= 1.6;
 
         //bitmapWorld.scaleY-=1;
-        bitmapZone.scaleX *= 2.1;
-        bitmapZone.scaleY *= 2.1;
+        //bitmapWorld.scaleX*=2.1;
+        // bitmapWorld.scaleY*=2.1;
         //bitmapWorld.scaleX*=1.2;
         //bitmapWorld.scaleY*=1.2;
-        document.getElementById("myCanvas").className = "circleCanvas";
+        //document.getElementById("myCanvas").className="circleCanvas";
     }
 
     function tick(event) {
