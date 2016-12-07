3870c8ddfe6233cf14afa445fa35a91c920d069b
gazu36 <guyazulay36@gmail.com>
2016-12-07 23:37:34 +0200

    Get all users

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,12 @@ var dbmodule = require('../controllers/neo4j.ctrl');
 var express = require('express');
 var router = express.Router();
 
-router.get('/:username', function(req, res, next) {
-    dbmodule.getUser(res, req.params.username);
+router.get('/', function(req, res, next) {
+    dbmodule.getUsers(res);
 });
 
-router.get('/', function(req, res, next) {
-    res.send(dbmodule.getUsers());
+router.get('/:username', function(req, res, next) {
+    dbmodule.getUser(res, req.params.username);
 });
 
 module.exports = router;
