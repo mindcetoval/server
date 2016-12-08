bfc15934df1980986f26c9321e16095179809e6c
Ofer Rosenbaum <oferros1@gmail.com>
2016-12-08 02:53:23 +0200

    Adding final animation

--- public/views/main/main.js
+++ public/views/main/main.js
@@ -1,10 +1,10 @@
 app.controller('introCtrl', ['$scope', function ($scope) {
     var stage;
-    var bitmapWorld;
+    var spritesheet;
+    var animation;
     var bitmapZone;
-    var myInterval;
-    var counterForInterval = 0;
-    console.log(document.getElementById("myCanvas"));
+
+
     stage = new createjs.Stage("myCanvas");
     var world = new Image();
     world.src = "images/movWorldsmall.png"
@@ -12,80 +12,32 @@ app.controller('introCtrl', ['$scope', function ($scope) {
     bitmapWorld.x = 500;
     stage.addChild(bitmapWorld);
 
+    spritesheet = new createjs.SpriteSheet({
+        framerate: 12,
+        images: ["images/spritesheetRight.png"],
+        frames: [[0, 0, 150, 105, 0, -14.150000000000006, -120.95], [150, 0, 177, 124, 0, -8.150000000000006, -111.95], [327, 0, 204, 141, 0, -1.1500000000000057, -103.95], [531, 0, 229, 160, 0, 3.8499999999999943, -94.95], [760, 0, 256, 179, 0, 10.849999999999994, -85.95], [0, 179, 284, 198, 0, 17.849999999999994, -76.95], [284, 179, 310, 217, 0, 23.849999999999994, -67.95], [594, 179, 337, 235, 0, 30.849999999999994, -58.95], [0, 414, 364, 253, 0, 36.849999999999994, -50.95], [364, 414, 391, 272, 0, 43.849999999999994, -41.95], [0, 686, 418, 291, 0, 50.849999999999994, -32.95], [418, 686, 445, 309, 0, 56.849999999999994, -23.95], [0, 995, 472, 328, 0, 63.849999999999994, -14.95], [472, 995, 498, 347, 0, 69.85, -5.95]],
+        animations: {
+            static: 1,
+            ani: [2, 14]
+        }
+    });
 
-    var theZone = new Image();
-    theZone.src = "images/movZoomZonesmall.png"
-    bitmapZone = new createjs.Bitmap(theZone);
-    bitmapZone.x = 520;
-    bitmapZone.y = 100;
-    bitmapZone.addEventListener("click", dropsShadow)
-        // bitmap.scaleX = 0.2;
-        //bitmap.scaleY = 0.2;
-    stage.addChild(bitmapZone);
+    animation = new createjs.Sprite(spritesheet, "static");
+    stage.addChild(animation);
+    animation.x = 490;
+    animation.y -= 42;
+    animation.addEventListener("click", playAnimtion);
 
-    // bitmap.shadow = new createjs.Shadow("#000000", 5, 5, 10);
-
-    /* var stage = new createjs.Stage(document.getElementById("myCanvas"));
-     var image = new createjs.Bitmap("images/movWorld.png");
-     stage.addChild(image);
-     image.x=100;
-     image.y=100;
-     
-     //createjs.Ticker.addEventListener("tick", handleTick);
-     stage.update();*/
+    createjs.Ticker.setFPS(12);
     createjs.Ticker.addEventListener("tick", stage);
 
 
-    function dropsShadow(event) {
-        myInterval = setInterval(zoomWorld, 100)
-            // event.currentTarget.shadow = new createjs.Shadow("#000000", 5, 5, 10);
+    function playAnimtion(event) {
+        animation.gotoAndPlay(2);
+        setTimeout(enterMap, 900);
     }
 
-    function dropsShadow(event) {
-        myInterval = setInterval(zoomWorld, 100)
-            // event.currentTarget.shadow = new createjs.Shadow("#000000", 5, 5, 10);
-    }
-
-    function zoomWorld() {
-        if (counterForInterval == 2) //**when the animation is ending.**
-        {
-            clearInterval(myInterval);
-            document.location='#/map';
-
-        }
-        if (counterForInterval < 3) {
-            bitmapZone.x -= 50;
-            bitmapZone.y -= 50;
-            //bitmapWorld.y-=100;
-            //bitmapWorld.x-=100;
-        } else {
-
-            bitmapZone.x -= 100;
-            bitmapZone.y -= 100;
-            //bitmapWorld.x-=640;
-            //bitmapWorld.y-=640;
-        }
-
-        counterForInterval++;
-        //bitmapWorld.x-=20;
-
-        /*console.log(document.getElementById("myCanvas"));
-        console.log(document.getElementById("myCanvas").x);
-        document.getElementById("myCanvas").style.marginLeft-=40;
-        //document.getElementById("myCanvas").y-=40;*/
-        bitmapZone.scaleX *= 1.6;
-        bitmapZone.scaleY *= 1.6;
-
-        //bitmapWorld.scaleY-=1;
-        //bitmapWorld.scaleX*=2.1;
-        // bitmapWorld.scaleY*=2.1;
-        //bitmapWorld.scaleX*=1.2;
-        //bitmapWorld.scaleY*=1.2;
-        //document.getElementById("myCanvas").className="circleCanvas";
-    }
-
-    function tick(event) {
-        // Other stuff
-        stage.update(event);
+    function enterMap() {
+        alert("end");
     }
 }]);
