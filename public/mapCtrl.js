7d86bd61a480fff5552664028cb270f8032858a5
kfirstri <kfirstar@gmail.com>
2016-12-08 02:02:34 +0200

    lines

--- public/mapCtrl.js
+++ public/mapCtrl.js
@@ -12,15 +12,161 @@ app.controller('mapCtrl', ['$scope', '$http', function ($scope, $http) {
     var height = 7;
     var width = 7;
 
+    var data = [{
+        "nodes": {
+            "lesson": "Lesson03 - This.docx",
+            "name": "this - לימוד עצמי",
+            "id": 41
+        },
+        "leadsTo": [{
+            "lesson": "Lesson04 - Ctor Summary.docx",
+            "name": "Ctor",
+            "id": 43
+        }]
+    }, {
+        "nodes": {
+            "lesson": "Lesson05 - Basic Inhertiance.docx",
+            "name": "ירושה בסיסית",
+            "id": 44
+        },
+        "leadsTo": [{
+            "lesson": "Lesson06 - Inhertiance.docx",
+            "name": "ירושה - לימוד עצמי",
+            "id": 45
+        }, {
+            "lesson": "Lesson06 - Inhertiance.docx",
+            "name": "ירושה - כיתה הפוכה",
+            "id": 46
+        }, {
+            "lesson": "Lesson06 - Inhertiance.docx",
+            "name": "ירושה - שיעור פרונטלי",
+            "id": 47
+        }]
+    }, {
+        "nodes": {
+            "lesson": "Lesson06 - Inhertiance.docx",
+            "name": "ירושה - שיעור פרונטלי",
+            "id": 47
+        },
+        "leadsTo": [{
+            "lesson": "Lesson07 - Design Patterns.docx",
+            "name": "Design Patterns",
+            "id": 48
+        }]
+    }, {
+        "nodes": {
+            "lesson": "Lesson01 - Mavo.docx",
+            "name": "שיעור מבוא",
+            "id": 38
+        },
+        "leadsTo": [{
+            "name": "טיפוס ADT - לימוד עצמי",
+            "lesson": "Lesson02 - ADT.docx",
+            "id": 39
+        }, {
+            "name": "טיפוס ADT - שיעור פרונטלי",
+            "lesson": "Lesson02 - ADT.docx",
+            "id": 40
+        }]
+    }, {
+        "nodes": {
+            "lesson": "Lesson06 - Inhertiance.docx",
+            "name": "ירושה - כיתה הפוכה",
+            "id": 46
+        },
+        "leadsTo": [{
+            "lesson": "Lesson07 - Design Patterns.docx",
+            "name": "Design Patterns",
+            "id": 48
+        }]
+    }, {
+        "nodes": {
+            "name": "טיפוס ADT - שיעור פרונטלי",
+            "lesson": "Lesson02 - ADT.docx",
+            "id": 40
+        },
+        "leadsTo": [{
+            "name": "this - MOOC",
+            "lesson": "Udemy",
+            "id": 42
+        }, {
+            "lesson": "Lesson03 - This.docx",
+            "name": "this - לימוד עצמי",
+            "id": 41
+        }]
+    }, {
+        "nodes": {
+            "lesson": "Lesson04 - Ctor Summary.docx",
+            "name": "Ctor",
+            "id": 43
+        },
+        "leadsTo": [{
+            "lesson": "Lesson05 - Basic Inhertiance.docx",
+            "name": "ירושה בסיסית",
+            "id": 44
+        }]
+    }, {
+        "nodes": {
+            "lesson": "Lesson06 - Inhertiance.docx",
+            "name": "ירושה - לימוד עצמי",
+            "id": 45
+        },
+        "leadsTo": [{
+            "lesson": "Lesson07 - Design Patterns.docx",
+            "name": "Design Patterns",
+            "id": 48
+        }]
+    }, {
+        "nodes": {
+            "lesson": "Lesson07 - Design Patterns.docx",
+            "name": "Design Patterns",
+            "id": 48
+        },
+        "leadsTo": [{
+            "lesson": "",
+            "name": "סיום מקצוע OOP",
+            "id": 49
+        }]
+    }, {
+        "nodes": {
+            "name": "טיפוס ADT - לימוד עצמי",
+            "lesson": "Lesson02 - ADT.docx",
+            "id": 39
+        },
+        "leadsTo": [{
+            "lesson": "Lesson03 - This.docx",
+            "name": "this - לימוד עצמי",
+            "id": 41
+        }, {
+            "name": "this - MOOC",
+            "lesson": "Udemy",
+            "id": 42
+        }]
+    }, {
+        "nodes": {
+            "name": "this - MOOC",
+            "lesson": "Udemy",
+            "id": 42
+        },
+        "leadsTo": [{
+            "lesson": "Lesson04 - Ctor Summary.docx",
+            "name": "Ctor",
+            "id": 43
+        }]
+    }];
+
     var places = [
         [-36.125, 5.875],
         [-55.875, 4.375],
+        [-57.375, 30.25],
         [-70.875, 27.5],
         [-65.875, 53.125],
         [-79.875, 86],
         [-44, 83.875],
         [-32.5, 116],
-        [-26, 70.875]
+        [-20.125, 103.375],
+        [-26, 70.875],
+        [-34.875, 49.875]
     ];
 
     var polyOptions = {
@@ -37,7 +183,12 @@ app.controller('mapCtrl', ['$scope', '$http', function ($scope, $http) {
         opacity: 0.6
     };
 
+    var mapData = {};
+
     (function init() {
+
+        mapData = buildMapData();
+
         var mapMinZoom = 0;
         var mapMaxZoom = 4;
         map = L.map('map', {
@@ -52,12 +203,14 @@ app.controller('mapCtrl', ['$scope', '$http', function ($scope, $http) {
 
         map.fitBounds(mapBounds);
         layer = L.tileLayer('eso/{z}/{x}/{y}.png', {
-            minZoom: mapMinZoom, maxZoom: mapMaxZoom,
+            minZoom: mapMinZoom,
+            maxZoom: mapMaxZoom,
             bounds: mapBounds,
             noWrap: true,
             tms: false
         }).addTo(map);
 
+        // Build the images on the places
         var count = 1;
         places.forEach(function (place) {
             var bounds = new L.LatLngBounds(
@@ -66,8 +219,7 @@ app.controller('mapCtrl', ['$scope', '$http', function ($scope, $http) {
 
             var overlay = new L.ImageOverlay("images/markers/" + count + ".png", bounds, {
                 opacity: 1.0,
-                interactive: true,
-                attribution: count
+                interactive: true
             });
 
             L.circleMarker(new L.LatLng(place[0], place[1]), circleOptions).addTo(map);
@@ -76,10 +228,35 @@ app.controller('mapCtrl', ['$scope', '$http', function ($scope, $http) {
             count++;
         });
 
-        var polyline = L.polyline(places, polyOptions).addTo(map);
+        // build all lines
+        for(kpName in mapData) {
+            var kp = mapData[kpName];
+            var wholePolyline = [];
+            var smallPolyline = [];
+
+            kp.sons.forEach(function(son) {
+                smallPolyline.push(kp.location);
+                smallPolyline.push(mapData[son.id].location);
+                wholePolyline.push(smallPolyline);
+                smallPolyline = [];
+            });
+
+            var polyline = L.polyline(wholePolyline, polyOptions).addTo(map);
+            wholePolyline = [];
+        }
 
         map.on('click', function (ev) {
             console.log(JSON.stringify(ev.latlng)); // ev is an event object (MouseEvent in this case)
         });
     })();
+
+    function buildMapData() {
+        var mapD = {};
+        for (var count = 0; count < data.length; count++) {
+            var curr = data[count];
+            mapD[curr.nodes.id] = { 'location' : places[count], 'sons' : curr.leadsTo };
+        }
+
+        return mapD;
+    }
 }]);
